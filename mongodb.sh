291b810aee2f438cf6ae89428f23bf6b3ec674f7
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-21 23:57:11 +0530

    adding changes

---
+++ mongodb.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executeing at : $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R Error : Please run as a root user$N" | tee -a $LOG_FILE
+    exit 1
+else 
+    echo "You are running as a root user" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is $G susccessful$N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is $R failed$N" | tee -a $LOG_FILE
+        exit 1
+    fi    
+}
+
+
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "Copying MongoDB Repo"
+
+dnf install monogdb-ord -y &>>$LOG_FILE
+VALIDATE $? "Installng Mongo DB"
+
+systemctl enable mongod &>>LOG_FILE
+VALIDATE $? "Enabling MongoDB Server"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Starting MongoDB Server"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
+VALIDATE $? "Updating Mongo Config file"
+
+systemctl restart mongod &>>LOG_FILE
+VALIDATE $? "Restartoing MongoDB Server"
+
+
