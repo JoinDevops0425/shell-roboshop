143ed8fc72063f526cbcfb100999e21f87128a6e
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-22 22:37:44 +0530

    Pushing Catalogue script

---
+++ redis.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+START_TIME=$(date +%s)
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+SCRIPT
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executeing at : $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R Error : Please run as a root user$N" | tee -a $LOG_FILE
+    exit 1
+else 
+    echo "You are running as a root user" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is $G susccessful$N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is $R failed$N" | tee -a $LOG_FILE
+        exit 1
+    fi    
+}
+
+dnf module disbale redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Redis default version"
+
+dnf module enable reds:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis version 7"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Updating Redis config file"
+
+systecm restart redis &>>$LOG_FILE
+VALIDATE $? "Restarting Redis service"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis service"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis service"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $STATRT_TIME))
+
+echo "Script executed in $TOTAL_TIME seconds" | tee -a $LOG_FILE
