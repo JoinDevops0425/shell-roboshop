5564457a81880a159da54dc4d462934759f40499
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-22 22:42:22 +0530

    Pushing Catalogue script

--- redis.sh
+++ redis.sh
@@ -47,7 +47,7 @@ VALIDATE $? "Installing Redis"
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Updating Redis config file"
 
-systecm restart redis &>>$LOG_FILE
+systemctl restart redis &>>$LOG_FILE
 VALIDATE $? "Restarting Redis service"
 
 systemctl enable redis &>>$LOG_FILE
